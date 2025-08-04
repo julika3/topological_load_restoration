d1904c0429351c8413183e7a1304e80908f75533
jdertnig <Julika.Dertnig@50hertz.com>
2025-08-05 01:22:01 +0200

    include creation of RestorationNetwork in main

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+from RestorationNetwork import RestorationNetwork
 from executables import *
+from constants import NETWORK_MODEL_NAME
 
 if __name__ == "__main__":
-    print('Starting Programme')
-    print('Create Network')
-    my_restoration_network = RestorationNetwork('50Hertz')
-    my_restoration_network.set_load_case(LOAD_CASE, scale=True)
-    run_superposition_restoration(my_restoration_network)
-    run_resilience_indicator_restoration(my_restoration_network)
-    run_scenario(my_restoration_network, build=True, node='Hamburg-Ost')
+    my_restoration_network = RestorationNetwork(NETWORK_MODEL_NAME)
